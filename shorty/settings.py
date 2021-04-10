768324007227c18fe158952acf030750908db24b
krisblarq <54292555+krisblarq@users.noreply.github.com>
2021-04-10 10:09:28 +0100

    Update settings.py

--- shorty/settings.py
+++ shorty/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATIC_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
