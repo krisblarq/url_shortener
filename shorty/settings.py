3277b9d01fbd9001ee915b255140c358f51e9bdf
krisblarq <naturalicon07@gmail.com>
2021-04-11 18:16:31 +0100

    change

--- shorty/settings.py
+++ shorty/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR, ]
 
-STATICFILES_DIRS = (
+STATIC_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
